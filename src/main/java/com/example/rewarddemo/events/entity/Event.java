80be6aea702681dd18e9fdee0e4d191d2a5a364a
jaewon.jung <jgd0948@naver.com>
2022-12-12 19:07:16 +0900

    feat: event entity 추가

---
+++ src/main/java/com/example/rewarddemo/events/entity/Event.java
@@ -0,0 +1,22 @@
+package com.example.rewarddemo.events.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event {
+    @Id
+    private String id;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+}
---
+++ src/test/java/com/example/rewarddemo/events/entity/EventTest.java
@@ -0,0 +1,31 @@
+package com.example.rewarddemo.events.entity;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EventTest {
+
+    @Test
+    @DisplayName("event 객체 생성 test")
+    public void javaBean() {
+        // given
+        String eventId = "reward";
+        String title = "매일 00시 00분 00초 선착순 10명 100 포인트 지급!!!";
+        String description = """
+                • 보상지급방식은사용자가받기를누를때지급하게 됩니다.
+                • 선착순 10 명에게 100 포인트의 보상이 지급 되며 10 명 이후에는 지급되지 않아야 합니다.
+                • 3일 연속,5일 연속,10일 연속 보상을 받는 경우 300 포인트, 500 포인트, 1,000 포인트를 추가 보상 받게 됩니다.
+                • 추가 보상은 10일 까지 이어지며 그 이후 연속 보상 횟수는 1 회 부터 다시 시작 됩니다.""";
+
+        // when
+        Event event = new Event(eventId, title, description);
+
+        // then
+        assertThat(event).isNotNull();
+        assertThat(event.getId()).isEqualTo(eventId);
+        assertThat(event.getTitle()).isEqualTo(title);
+        assertThat(event.getDescription()).isEqualTo(description);
+    }
+}
