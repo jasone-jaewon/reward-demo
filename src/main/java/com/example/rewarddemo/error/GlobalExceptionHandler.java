7d28a5b2207ce816406b2d0d07395627ad8b98a1
jaewon.jung <jgd0948@naver.com>
2022-12-16 16:51:24 +0900

    feat: GlobalExceptionHandler 추가

---
+++ src/main/java/com/example/rewarddemo/error/GlobalExceptionHandler.java
@@ -0,0 +1,46 @@
+package com.example.rewarddemo.error;
+
+import com.example.rewarddemo.error.exception.BadRequestException;
+import com.example.rewarddemo.error.exception.NotFoundException;
+import com.example.rewarddemo.error.exception.UnAuthorizedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler({IllegalArgumentException.class, BadRequestException.class})
+    public ResponseEntity<Error> handleBadRequest(RuntimeException exception) {
+        String message = exception.getMessage();
+        HttpStatus badRequest = HttpStatus.BAD_REQUEST;
+        Error error = new Error(badRequest, message);
+        return ResponseEntity.badRequest().body(error);
+    }
+
+    @ExceptionHandler({NotFoundException.class})
+    public ResponseEntity<Error> handleNotFound(RuntimeException exception) {
+        String message = exception.getMessage();
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        Error error = new Error(status, message);
+        return ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler({UnAuthorizedException.class})
+    public ResponseEntity<Error> handleUnAuthorized(RuntimeException exception) {
+        String message = exception.getMessage();
+        HttpStatus status = HttpStatus.UNAUTHORIZED;
+        Error error = new Error(status, message);
+        return ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<Error> handleUnhandledException(Exception exception) {
+        String message = exception.getMessage();
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        Error error = new Error(status, message);
+        return ResponseEntity.internalServerError().body(error);
+    }
+}
