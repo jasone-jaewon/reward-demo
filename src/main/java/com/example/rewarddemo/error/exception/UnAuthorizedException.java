78f3b4e1ee0a421dbfe22c0568f96fe4cb8de8f7
jaewon.jung <jgd0948@naver.com>
2022-12-16 16:50:56 +0900

    feat: http status 별 공통 excpetion 추가

---
+++ src/main/java/com/example/rewarddemo/error/exception/UnAuthorizedException.java
@@ -0,0 +1,8 @@
+package com.example.rewarddemo.error.exception;
+
+public class UnAuthorizedException extends RuntimeException {
+
+    public UnAuthorizedException(String message) {
+        super(message);
+    }
+}
