2dd4738f68b19237627c4ac8f39ef3065484425d
jaewon.jung <jgd0948@naver.com>
2022-12-16 21:50:58 +0900

    feat: EventNotFoundException 상속 변경

--- src/main/java/com/example/rewarddemo/events/exception/EventNotFoundException.java
+++ src/main/java/com/example/rewarddemo/events/exception/EventNotFoundException.java
@@ -1,8 +1,10 @@
 package com.example.rewarddemo.events.exception;
 
+import com.example.rewarddemo.error.exception.NotFoundException;
+
 import java.text.MessageFormat;
 
-public class EventNotFoundException extends RuntimeException {
+public class EventNotFoundException extends NotFoundException {
 
     public EventNotFoundException(String eventId) {
         super(MessageFormat.format("이벤트 정보를 찾을 수 없습니다. event id: {0}", eventId));
